c94e92006df4e18d2bdd9d9e19cae0db2f37d7de
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-23 23:34:08 +0300

    MessageView Edited

---
+++ ChatAPP-SwiftUI/Models/Message.swift
@@ -0,0 +1,18 @@
+//
+//  Message.swift
+//  ChatAPP-SwiftUI
+//
+//  Created by Burak Ercikti on 23.01.2024.
+//
+
+import SwiftUI
+
+struct Message: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Message()
+}
