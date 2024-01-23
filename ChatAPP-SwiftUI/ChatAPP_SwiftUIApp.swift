7eacd0e1f17e0bdb63b553f249e1a6425fa73466
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-23 22:12:05 +0300

    Initial Commit

---
+++ ChatAPP-SwiftUI/ChatAPP_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAPP_SwiftUIApp.swift
+//  ChatAPP-SwiftUI
+//
+//  Created by Burak Ercikti on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAPP_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
